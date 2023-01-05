e375dd0c18ecdef86a74aba565e54e7a18e0b6e3
chenlotan <chenlotan@mail.tau.ac.il>
2023-01-05 19:14:32 +0200

    final

--- pfind.c
+++ pfind.c
@@ -8,11 +8,18 @@
 #include <stdatomic.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+// #define DEBUGM
 
+#ifdef DEBUGM
+# define DEBUG_PRINT(x) printf x
+#else
+# define DEBUG_PRINT(x) do {} while (0)
+#endif
 
 struct node
 {
     cnd_t wait;
+    thrd_t id;
     char dir_name[PATH_MAX];
     struct node* next; 
 };
@@ -32,8 +39,10 @@ int create_new_dir_node(struct node* new, char *name){
     return 0;
 }
 
-int create_new_thread_node(struct node* new){
+int create_new_thread_node(struct node* new, thrd_t id){
     new->next = NULL;
+    new->id = id;
+    DEBUG_PRINT(("cnd when creating node = %p\n", &new->wait));
     cnd_init(&new->wait);
     return 0;
 }
@@ -59,9 +68,8 @@ int enqueue(struct queue* queue, struct node* new){
 }
 
 char *dir_dequeue(struct queue* queue, char name[]){
-    struct node* tmp = queue->first;
     if (queue->length == 0) return NULL;
-    strcpy(name, tmp->dir_name);
+    strcpy(name, queue->first->dir_name);
 
     /*if there is one node in the queue*/
     if (queue->length == 1){
@@ -71,12 +79,11 @@ char *dir_dequeue(struct queue* queue, char name[]){
     queue->first = queue->first->next;
     --queue->length;
 
-    free(tmp);
     return name;
 }
 
 int thread_dequeue(struct queue *queue){
-    struct node* tmp = queue->first;
+    struct node *tmp;
     if (queue->length == 0) return -1;
 
     /*if there is one node in the queue*/
@@ -84,12 +91,14 @@ int thread_dequeue(struct queue *queue){
         queue->last = NULL;
     }
 
+    tmp = queue->first;
+
     queue->first = queue->first->next;
     --queue->length;
 
-    cnd_signal(&tmp->wait);
-    cnd_destroy(&tmp->wait);
-    free(tmp);
+    tmp->next = NULL;
+
+    
     return 0;
 }
 
@@ -101,35 +110,47 @@ int isEmpty(struct queue* queue){
     return 1;
 }
 
+void print_threads_queue(struct queue* queue){
+    DEBUG_PRINT(("----------------threads queue-----------------\n"));
+    struct node* curr = queue->first;
+    if (queue->length == 0) return;
+    while (curr != NULL)
+    {
+        DEBUG_PRINT(("thread id = %ld, cnd add = %p\n", curr->id, &(curr->wait)));
+        curr = curr->next;
+    }
+    return;
+}
 
-int num_threads;
+
+void print_dir_queue(struct queue* queue){
+    DEBUG_PRINT(("----------------dir queue-----------------\n"));
+    struct node* curr = queue->first;
+    if (queue->length == 0) return;
+    while (curr != NULL)
+    {
+        DEBUG_PRINT(("dir = %s\n", curr->dir_name));
+        curr = curr->next;
+    }
+    return;
+}
+
+int NUM_THREADS;
 char *file_name;
 atomic_int running_threads = 0;
 atomic_int files_counter = 0;
 int finished = 0;
-int start_search = 0;
-static struct queue *dir_queue;
-static struct queue *threads_queue;
-mtx_t tlock;
+int failed_flag = 0; /*indicates if one of the threads had error in run time*/
+struct queue *dir_queue;
+struct queue *threads_queue;
 
-int sync_threads_start(struct node *thread_node){
-    running_threads++;
-    if (running_threads == num_threads){
-        free(thread_node);
-        return 0;
-    }
-    else{
-        create_new_thread_node(thread_node);
-        enqueue(threads_queue, thread_node);
-        cnd_wait(&thread_node->wait, &tlock);
-    }
-    return 0;
-}
-
+mtx_t qlock;
+cnd_t start_searching;
+cnd_t first_thread;
 
 
 int wake_up_all_threads(){
-    struct node *curr;
+    struct node *curr, *tmp;
     if (threads_queue->length == 0){
         return 0;
     }
@@ -137,7 +158,9 @@ int wake_up_all_threads(){
         curr = threads_queue->first;
         while(curr != NULL){
             curr = curr->next;
+            tmp = threads_queue->first;
             thread_dequeue(threads_queue);
+            cnd_signal(&tmp->wait);
             }
     }
     return 0;
@@ -151,42 +174,54 @@ int search_in_dir(char *dir_name){
     DIR *dir;
     struct dirent* entry;
     struct stat path_stat;
+    struct node *tmp;
 
-    // printf("~~~~~~~~~~~~~~~~~~~dir name = %s~~~~~~~~~~~~~~~~~~~~~~~~~\n", dir_name);
+    DEBUG_PRINT(("~~~~~~~~~~~~~~~~~~~~running thread %ld~~~~~~~~~~~~~~~~~~~~\n", thrd_current()));
+    DEBUG_PRINT(("~~~~~~~~~~~~~~~~~~~~~~dir name = %s~~~~~~~~~~~~~~~~~~~~~~~~\n", dir_name));
 
     dir = opendir(dir_name);
 
     if (dir == NULL){
-        fprintf(stderr, "ERROR: opendir()\n"); /*not suppose to happen*/
+        fprintf(stderr, "ERROR: opendir()\n"); /*not suppose to happen since we check access() before adding to queue*/
+        failed_flag = 1;
         return 1;
     }
 
 
     while((entry = readdir(dir)) != NULL){
-        // printf("-------entry_name = %s----------\n", entry->d_name);
+        // DEBUG_PRINT(("-------entry_name = %s----------\n", entry->d_name));
         sprintf(new_dir_name, "%s/%s", dir_name, entry->d_name);
         
         if (stat(new_dir_name, &path_stat) != 0) {
-            fprintf(stderr, "ERROR stat = %d on dir = %s !\n", stat(new_dir_name, &path_stat), new_dir_name);
+            fprintf(stderr, "ERROR in stat()");
+            failed_flag = 1;
         };
 
         if (S_ISDIR(path_stat.st_mode) != 0){
-            // printf("directory\n");
             if (access(new_dir_name, R_OK | X_OK) != 0){
                 printf("Directory %s: Permission denied.\n", new_dir_name);
             }
             else{
                 if ((strcmp(entry->d_name, ".") != 0) & (strcmp(entry->d_name, "..") !=0)){
-                    mtx_lock(&tlock);
-                    struct node* new = (struct node*) malloc(sizeof(struct node));
-                    if (new == NULL){
-                        fprintf(stderr, "ERROR: allocation failed.\n");
+                    mtx_lock(&qlock);
+                    if (threads_queue->length == 0){
+                        struct node* new = (struct node*) malloc(sizeof(struct node));
+                        if (new == NULL) {
+                            fprintf(stderr, "ERROR: allocation failed.\n");
+                            failed_flag = 1;
+                            }
+                        create_new_dir_node(new, new_dir_name);
+                        enqueue(dir_queue, new); /*if there is no thread waiting, put dir in queue*/
                     }
-                    // printf("add dir = %s\n", new_dir_name);
-                    create_new_dir_node(new, new_dir_name);
-                    enqueue(dir_queue, new);
-                    thread_dequeue(threads_queue);
-                    mtx_unlock(&tlock);
+                    else{
+                        DEBUG_PRINT(("pass task to waiting thread %ld, dir name = %s\n", threads_queue->first->id, new_dir_name));
+                        tmp = threads_queue->first;
+                        strcpy(tmp->dir_name, new_dir_name); /*if there is a waiting thread - give the task to him*/
+                        thread_dequeue(threads_queue);
+                        cnd_signal(&(tmp->wait));
+                    }
+                    mtx_unlock(&qlock);
+                    
 
                 }
             }
@@ -206,52 +241,64 @@ int search_in_dir(char *dir_name){
 
 
 int thread_search(void *arg){
-    struct node *thread_node; 
     char dir_name[PATH_MAX];
-    
-    mtx_lock(&tlock);
-    thread_node = (struct node*) malloc(sizeof(struct node));
-    sync_threads_start(thread_node);
-    mtx_unlock(&tlock);
 
-    // printf("before while %ld\n", thrd_current());
+    struct node *thread_node = (struct node*) malloc(sizeof(struct node));
+    if (thread_node == NULL){
+        fprintf(stderr, "ERROR: allocation failed.\n");
+        failed_flag = 1;
+    }
 
+    create_new_thread_node(thread_node, thrd_current());
     
+    mtx_lock(&qlock);
+    running_threads++;
+    cnd_wait(&start_searching, &qlock);
+    mtx_unlock(&qlock);
+    
+
+    DEBUG_PRINT(("before while %ld\n", thrd_current()));    
+    running_threads++;
     while (1)
-    {            
-        mtx_lock(&tlock);
-        while (isEmpty(dir_queue))
-        {
-            // printf("number of waiting threads %d, num_threads %d\n", threads_queue->length, num_threads);
-            // printf("dir queue length = %d , first = %p \n", dir_queue->length, dir_queue->first);
-            if ((threads_queue->length >= (num_threads - 1)) & isEmpty(dir_queue) & !finished){
-                wake_up_all_threads();
-                finished = 1;
-                // printf("---------finished search ----------\n");
-            }
-            if (finished){
-                mtx_unlock(&tlock);
-                thrd_exit(0);
-            }
-            thread_node = (struct node*) malloc(sizeof(struct node));
-            create_new_thread_node(thread_node);
-            enqueue(threads_queue, thread_node);
-            // printf("add to queue thread %ld\n", thrd_current());
-            cnd_wait(&thread_node->wait, &tlock);
+    {
+        print_threads_queue(threads_queue);
+        print_dir_queue(dir_queue);
+
+        mtx_lock(&qlock);
+        if (!isEmpty(dir_queue)){
+        // if ((NUM_THREADS - threads_queue->length) <= dir_queue->length){
+            dir_dequeue(dir_queue, dir_name);
         }
         
-                
-        // printf("%ld\n", thrd_current());
-        start_search = 1;
-        // printf("after dequeue of dir\n");
-        dir_dequeue(dir_queue, dir_name);
-        mtx_unlock(&tlock);
-       
+        else{
+            if ((threads_queue->length == (NUM_THREADS - 1)) & isEmpty(dir_queue)){
+                finished = 1;
+                wake_up_all_threads();              
+                DEBUG_PRINT(("---------finished search ----------\n"));
+                cnd_destroy(&thread_node->wait);
+                mtx_unlock(&qlock);
+                thrd_exit(0);
+            }
 
-        // printf("waiting threads = %d\n", threads_queue->length);
+            enqueue(threads_queue, thread_node);
+            DEBUG_PRINT(("add to queue thread %ld\n", thrd_current()));
 
-        search_in_dir(dir_name);
+            cnd_wait(&(thread_node->wait), &qlock);
+
+            if (finished){
+                cnd_destroy(&thread_node->wait);
+                mtx_unlock(&qlock);
+                thrd_exit(0);
+            }
+
+            strcpy(dir_name, thread_node->dir_name);
+            
+        }
+      
+        mtx_unlock(&qlock);
+
         
+        search_in_dir(dir_name);     
         
     }
 
@@ -259,13 +306,8 @@ int thread_search(void *arg){
 
 
 
-
-
-
 int main(int argc, char *argv[]){
-    int status, all_success, rc, t;
-
-    all_success = 0;
+    int rc, t;
 
     if (argc != 4){
         fprintf(stderr, "wrong amount of arguments\n");
@@ -278,9 +320,9 @@ int main(int argc, char *argv[]){
     }
 
 
-    num_threads = atoi(argv[3]);
+    NUM_THREADS = atoi(argv[3]);
     file_name = argv[2];
-    thrd_t thread_id[num_threads];
+    thrd_t thread_id[NUM_THREADS];
 
 
     dir_queue = (struct queue*) malloc(sizeof(struct queue));
@@ -294,7 +336,9 @@ int main(int argc, char *argv[]){
     create_new_queue(dir_queue);
     create_new_queue(threads_queue);
 
-    mtx_init(&tlock, mtx_plain);
+    mtx_init(&qlock, mtx_plain);
+    cnd_init(&start_searching);
+    cnd_init(&first_thread);
     
     struct node* new = (struct node*) malloc(sizeof(struct node));
     create_new_dir_node(new, argv[1]);
@@ -303,36 +347,40 @@ int main(int argc, char *argv[]){
     }
 
 
-    for (t = 0; t < num_threads; ++t) {
+    for (t = 0; t < NUM_THREADS; ++t) {
         rc = thrd_create(&thread_id[t], thread_search, NULL);
         if (rc != thrd_success) {
-            printf("ERROR in thrd_create()\n");
+            fprintf(stderr, "ERROR in thrd_create()\n");
             exit(1);
         }
     }
 
     
+    while(running_threads != NUM_THREADS) {thrd_yield();}
 
-    for (t = 0; t < num_threads; ++t) {
-        rc = thrd_join(thread_id[t], &status);
+    mtx_lock(&qlock);
+    running_threads = 0;
+    cnd_broadcast(&start_searching);
+    mtx_unlock(&qlock);
+
+    for (t = 0; t < NUM_THREADS; ++t) {
+        rc = thrd_join(thread_id[t], NULL);
         if (rc != thrd_success) {
-            printf("ERROR in thrd_join()\n");
+            fprintf(stderr,"ERROR in thrd_join()\n");
             exit(1);
         }
-        if (status != 0){
-            all_success = 1;
-        }
+        
     }
 
     printf("Done searching, found %d files\n", files_counter);
 
 
-    mtx_destroy(&tlock);
+    mtx_destroy(&qlock);
+    cnd_destroy(&start_searching);
+    cnd_destroy(&first_thread);
     free(dir_queue);
     free(threads_queue);
-
-
-    thrd_exit(all_success);
+    exit(failed_flag);
 
 
 }
